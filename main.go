1402dd86348d57ada611ed5a4fee06f6db06ab66
Stepanov2221 <step2221@gmail.com>
2020-12-13 13:02:48 +0500

    Конструктор структуры и метод структуры

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type employee struct {
+	name   string
+	sex    string //пол
+	age    int
+	salary int
+}
+
 func main() {
 	/*ages := make(map[string]int)
 	ages["Ксюша"] = 7
@@ -29,16 +36,23 @@ func main() {
 		fmt.Printf("Антона %d лет\n", age)
 	}
 
-	employee := struct {
-		name   string
-		sex    string
-		age    int
-		salary int
-	}{
-		name:   "Вася",
-		sex:    "М",
-		age:    25,
-		salary: 1500,
+	employee1 := newEmployee("Вася", "М", 25, 1500)
+	employee2 := newEmployee("Петя", "М", 28, 2000)
+
+	fmt.Printf("%v\n", employee1.getInfo())
+	fmt.Printf("%v\n", employee2.getInfo())
+	//fmt.Printf("%v\n", employee2)
+}
+
+func newEmployee(name, sex string, age, salary int) employee {
+	return employee{
+		name:   name,
+		sex:    sex,
+		age:    age,
+		salary: salary,
 	}
-	fmt.Printf("%v\n", employee)
+}
+
+func (e employee) getInfo() string {
+	return fmt.Sprintf("Сотрудник: %s\n Возраст: %d\n Зарплата: %d\n", e.name, e.age, e.salary)
 }
