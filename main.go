9ff2aa0390ad1a29a46bdad6deb80f5b0cbcb81d
Stepanov2221 <step2221@gmail.com>
2020-12-13 11:16:16 +0500

    Условные операто

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ const pi float64 = 3.1415
 
 func main() {
 	PrintCircleArea(2)
-	PrintCircleArea(4)
+	PrintCircleArea(-2)
 
 	fmt.Println("Площадь круга c радиусом 5см=", CalculateCircleArea(5))
 }
@@ -18,6 +18,12 @@ func CalculateCircleArea(radius int) float64 {
 
 //PrintCircleArea Вывод в терминал
 func PrintCircleArea(radius int) {
+
+	if radius <= 0 {
+		fmt.Println("Радиус круга не может быть отрицательным!")
+		return
+	}
+
 	fmt.Printf("Радиус круга: %d см. \n", radius)
 	fmt.Printf("Формула для расчета круга: A=пr2\n")
 
