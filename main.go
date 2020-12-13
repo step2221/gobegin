8a1ab246ef036172129fa812c5a833fb90198fdf
Stepanov2221 <step2221@gmail.com>
2020-12-13 12:34:33 +0500

    Инициализация мапы с набором значений

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	ages := make(map[string]int)
+	/*ages := make(map[string]int)
 	ages["Ксюша"] = 7
 	ages["Андрей"] = 5
 	ages["Саша"] = 8
 
 	for key, value := range ages {
 		fmt.Printf("%s - %d\n", key, value)
+	}*/
+	ages := map[string]int{
+		"Ксюша":  7,
+		"Андрей": 5,
+		"Саша":   8,
 	}
+
+	fmt.Printf("Ксюше %d лет\n", ages["Ксюша"])
 }
