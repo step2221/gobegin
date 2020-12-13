7e534e99764fec226b6f167e6908d1df249f2e2d
Stepanov2221 <step2221@gmail.com>
2020-12-13 11:40:32 +0500

    Массив, for..range

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-const pi float32 = 3.1415
-
 func main() {
-
-	PrintCircleArea(-2)
-
-}
-
-//CalculateCircleArea ..Рассчитывает площадь окружности
-func CalculateCircleArea(radius int) (float32, error) {
-	if radius <= 0 {
-		return float32(0), errors.New("радиус круга не может быть отрицательным")
+	var todoList = [3]string{
+		"пункт 1", "пункт 2", "пункт 3",
 	}
-	return pi * (float32(radius) * float32(radius)), nil
-}
-
-//PrintCircleArea Вывод в терминал
-func PrintCircleArea(radius int) {
-
-	circleArea, err := CalculateCircleArea(radius)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	fmt.Printf("Кол-во элементов в списке: %d \n", len(todoList))
+	for index, item := range todoList {
+		fmt.Printf("%d. %s \n", index, item)
 	}
-
-	fmt.Printf("Радиус круга: %d см. \n", radius)
-	fmt.Printf("Формула для расчета круга: A=пr2\n")
-	fmt.Printf("Площадь круга: %f32 см. кв.\n", circleArea)
 }
