90e3177c5b7ab240d7637b23a646b8179e017733
Stepanov2221 <step2221@gmail.com>
2020-12-13 11:47:18 +0500

    Варианты объявления массивов

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 )
 
 func main() {
-	var todoList = [3]string{
+	/*var todoList = [3]string{
 		"пункт 1", "пункт 2", "пункт 3",
-	}
+	}*/
+	//Либо так ...
+	/*var todoList [3]string
+	todoList[0] = "пункт 1"*/
+	//Либо так ...
+	var todoList = [...]string{"пункт 1", "пункт 2", "пункт 3"}
+
 	fmt.Printf("Кол-во элементов в списке: %d \n", len(todoList))
-	for index, item := range todoList {
+	/*for index, item := range todoList {
 		fmt.Printf("%d. %s \n", index, item)
+	}*/
+	for _, item := range todoList {
+		fmt.Printf("%s\n", item)
 	}
 }
